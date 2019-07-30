f279b07e95bd2ebd184ab6e78769a44b7fa788d9
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-07-31 00:40:03 +0530

    Routing created

--- front-end/src/components/Index.js
+++ front-end/src/components/Index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-class App extends React.Component {
+export default class Index extends React.Component {
 
     render() {
         return (
             <div>
-                {this.props.children}
+                Index
             </div>
         );
     }
-}
 
-export default App;
+}
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import App from './App';
+import Index from './components/Index'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Index}/>
+            </Switch>
+        </App>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 serviceWorker.unregister();
