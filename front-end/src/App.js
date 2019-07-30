09c0c4284d95ae4372434167f3d9db7251a8e8f3
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-07-31 00:53:21 +0530

    Appbar added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Appbar from './components/Appbar';
 
 class App extends React.Component {
 
     render() {
         return (
             <div>
+                <Appbar/>
                 {this.props.children}
             </div>
         );
