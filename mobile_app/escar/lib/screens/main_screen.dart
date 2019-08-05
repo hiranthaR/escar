7f56151bbc717b136c6bab7cc069247c9c0fdbde
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-08-05 11:05:31 +0530

    androidX migrated and multidex anabaled

--- mobile_app/escar/lib/screens/main_screen.dart
+++ mobile_app/escar/lib/screens/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -6,13 +8,44 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  var barcode = '';
+  var details = '';
+
+  onScan() async {
+    try {
+      String barcode =
+          await FlutterBarcodeScanner.scanBarcode('#34eb7d', 'cancel', true);
+      setState(() {
+        this.barcode = barcode;
+      });
+      // QuerySnapshot qs = await Firestore.instance
+      //     .collection('items')
+      //     .where('barcode', isEqualTo: barcode)
+      //     .limit(1)
+      //     .getDocuments();
+      // setState(() {
+      //   details = qs.documents[0].data.toString();
+      // });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
         child: Column(
-          children: <Widget>[],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("click"),
+              onPressed: () => onScan(),
+            ),
+            Text(barcode),
+            Text(details),
+          ],
         ),
       ),
     );
