424a6da0397c7ae0ac24a83a8c60e0c62fbac32d
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-08-11 20:45:31 +0530

    config with dot env

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import App from './App';
 import Index from './components/Index'
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config();
+
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Switch>
-                <Route exact path="/" component={Index}/>
+                <Route exact path="/" component={Index} />
             </Switch>
         </App>
     </BrowserRouter>
